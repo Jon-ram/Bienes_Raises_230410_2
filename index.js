8be082620aced2410227e63e7c6cb30ce98c2702
unknown <230410@utxicotepec.edu.mx>
2024-11-23 09:46:04 -0600

    Practica 12
    Models,env y controllers

--- index.js
+++ index.js
@@ -2,10 +2,24 @@
 import express from 'express';
 import generalRoutes from './routes/generalRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-
-
+import db from './db/config.js'
 
 const app = express();
+
+//Conexion a la base de datos 
+try {
+   await db.authenticate(); //Verifico las credenciales del usuario
+   db.sync(); // Sincroniza las tablas 
+   console.log("Conexión establecida");
+
+}catch (error) {
+   console.log(error)
+}
+
+
+//Habilitando la lectura de datos del formulario
+app.use(express.urlencoded({ extended: true }));
+
 //configurar tempñate engine
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -13,6 +27,11 @@ app.use(express.static('./public'))
 const port = 3000;
 app.listen(port, ()=>{
    console.log(`La aplicación ha iniciado en el puesto: ${port}`);
+})
+
+// Probamos las rutas para poder presentar mensajes al usuario a través del navegador
+app.get("/",function(req,res){
+   res.send("Hola Mundo desde node, a traves del navegador")
 })
 
 app.use("/", generalRoutes);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,18 @@ import express from 'express';
 import { formularioLogin,formularioRegister,formularioPasswordRecovery } from '../controllers/userController.js';
 const router = express.Router();
 
+router.get("/busquedaPorID/:id", function (request,response){
+    response.send(`Se esta solicitando buscar al usuario con ID: ${request.params.id}`);
+})  // 2 componentes de una petición ruta, función callback 
+
+router.post("/newUser", createNewUser)
+
+
+
+
+
+
+
 //GET
 router.get("/FindById/:Id", function(request, response){
     response.send(`Se esta solicitando buscar al usuario con ID: ${request.params.Id}`);
@@ -14,28 +26,37 @@ router.post("/newUser/:name/:email/:password",function(req, res){
 })
 
 //PUT
-router.put("/replaceUser/:name/:email/:password", function(a,b){
-    b.send(`Se esta solicitando el remplazo de toda la informacion de usuario ${a.params.name},
-        con el correo electronico ${a.params.email} y la contraseña ${a.params.password}`);
-}) 
+router.put("/replaceUserByEmail/:name/:email/:password", function(a,b){
+    b.send(`Se ha solicitado el remplazo de toda la información del usuario ${a.params.name},
+     con correo: ${a.params.email} y contraseña: ${a.params.password}`)
+})
 
 //PATCH
-router.patch("/updatePassword/:email/:newPassword/:passwordConfirm", function(a, b){
-    const {email, newPassword, passwordConfirm} = a.params
-    if(newPassword===passwordConfirm){
-        b.send(`Se esta solicitando la actualizacion de la contraseña del usuario con el correo ${email}, se aceptan los cambios ya que
-            que la contraseña y la confirmacion es la misma`);
-    }else{
-        b.send(`Se esta solicitando la actualizacion de la contraseña del usuario con sol correo ${email},
-            con la nueva contraseña ${newPassword}, pero se rechazo el cambio dado que su nueva contraseña y su confirmacion no coinciden `);
+router.patch("/updatePassword/:email/:newPassword/:newPasswordConfirm", function(request, response){
+    const {email, newPassword, newPasswordConfirm} = request.params // Desestructuración de un objeto
+
+    if(newPassword == newPasswordConfirm)
+    {
+        response.send(`Se ha solicitado la actualización de la contraseña del usuario con correo: ${email}, se aceptan los cambios ya que la contraseña y confirmación son la misma.`)
+        console.log(newPassword);
+        console.log(newPasswordConfirm);
+    }
+    else
+    {    
+        response.send(`Se ha solicitado la actualización de la contraseña del usuario con correo: ${email} con la nueva contraseña ${newPassword}, pero se rechaza el cambio dado que la nueva contraseña y su confirmación no coinciden.`)
+        console.log(newPassword);
+        console.log(newPasswordConfirm);
     }
 })
 
 //DELETE
-router.delete("/deleteUser/:name", function(a,b){
-    b.send(`Se esta solicitando eliminar el usuario ${a.params.name}`);
+router.delete("/deleteUser/:email", function(request, response){
+    response.send(`Se ha solicitado la eliminación del usuario asociado al correo: ${request.params.email}`)
 })
+
+
 router.get("/login", formularioLogin)
 router.get("/register",formularioRegister)
 router.get("/passwordRecovery",formularioPasswordRecovery)
+router.get("/confirmAccount/:token",confirm) 
 export default router;
